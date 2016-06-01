3f95231b4d6880c05e68920aad92c7de5c6a4e49
lurenjia <yanshengjia1995@163.com>
2016-06-01 15:56:47 +0800

    Update

--- seu-lex-yacc/yacc/allcontrol.py
+++ seu-lex-yacc/yacc/allcontrol.py
@@ -2,7 +2,7 @@
 # LR 总控程序(查表程序)
 # Created by Shengjia Yan@2016-5-30
 
-# # 表达式文法
+# # 不带'|'的表达式文法 非终结符和终结符长度都为1
 # grammar_rule = [
 #     ['E', ['E', '+', 'T']],   # 0
 #     ['E', ['T']],             # 1
@@ -12,17 +12,27 @@
 #     ['F', ['i']]              # 5
 # ]
 
-# 表达式文法
+# # 不带'|'的表达式文法 非终结符和终结符长度大于1 
+# grammar_rule = [
+#     ['E1', ['E1', '+', 'T']],   # 0
+#     ['E1', ['T']],              # 1
+#     ['T', ['T', '*', 'F']],     # 2
+#     ['T', ['F']],               # 3
+#     ['F', ['(', 'E1', ')']],    # 4
+#     ['F', ['i']]                # 5
+# ]
+
+# 带'|'的表达式文法 非终结符和终结符长度大于1 
 grammar_rule = [
-    ['E1', ['E1', '+', 'T']],   # 0
-    ['E1', ['T']],              # 1
-    ['T', ['T', '*', 'F']],     # 2
-    ['T', ['F']],               # 3
-    ['F', ['(', 'E1', ')']],    # 4
-    ['F', ['i']]                # 5
+    ['E1', ['E1', '+', 'T']],           # 0  E1->E1+T
+    ['E1', ['T']],                      # 1  E1->T
+    ['T', ['T', '*', 'F']],             # 2  T->T*F
+    ['T', ['F']],                       # 3  T->F
+    ['F', ['(', 'E1', ')'], ['name']],     # 4  F->(E1)|i
 ]
 
-# # LR 分析表
+
+# # LR 分析表 非终结符和终结符长度都为1
 # parsing_table = {
 #     0:{'ACTION':{'i':'S5', '(':'S4'}, 'GOTO':{'E':1, 'T':2, 'F':3}},
 #     1:{'ACTION':{'+':'S6', '#':'acc'}, 'GOTO':{}},
@@ -38,7 +48,7 @@ grammar_rule = [
 #     11:{'ACTION':{'+':'r4', '*':'r4', ')':'r4', '#':'r4'}, 'GOTO':{}}
 # }
 
-# # LR 分析表
+# # LR 分析表 非终结符长度为1，终结符长度大于1
 # parsing_table = {
 #     0:{'ACTION':{'name':'S5', '(':'S4'}, 'GOTO':{'E':1, 'T':2, 'F':3}},
 #     1:{'ACTION':{'+':'S6', '#':'acc'}, 'GOTO':{}},
@@ -54,14 +64,14 @@ grammar_rule = [
 #     11:{'ACTION':{'+':'r4', '*':'r4', ')':'r4', '#':'r4'}, 'GOTO':{}}
 # }
 
-# LR 分析表
+# LR 分析表 非终结符和终结符长度都大于1
 parsing_table = {
     0:{'ACTION':{'name':'S5', '(':'S4'}, 'GOTO':{'E1':1, 'T':2, 'F':3}},
     1:{'ACTION':{'+':'S6', '#':'acc'}, 'GOTO':{}},
     2:{'ACTION':{'+':'r1', '*':'S7', ')':'r1', '#':'r1'}, 'GOTO':{}},
     3:{'ACTION':{'+':'r3', '*':'r3', ')':'r3', '#':'r3'}, 'GOTO':{}},
     4:{'ACTION':{'name':'S5', '(':'S4'}, 'GOTO':{'E1':8, 'T':2, 'F':3}},
-    5:{'ACTION':{'+':'r5', '*':'r5', ')':'r5', '#':'r5'}, 'GOTO':{}},
+    5:{'ACTION':{'+':'r4', '*':'r4', ')':'r4', '#':'r4'}, 'GOTO':{}},
     6:{'ACTION':{'name':'S5', '(':'S4'}, 'GOTO':{'T':9, 'F':3}},
     7:{'ACTION':{'name':'S5', '(':'S4'}, 'GOTO':{'F':10}},
     8:{'ACTION':{'+':'S6', ')':'S11'}, 'GOTO':{}},
@@ -123,7 +133,7 @@ def allControl(input_stack):
             print 'Analyse Successfully!'
             break
 
-        action = symbol_in_table[0]          # Shift or Reduce 
+        action = symbol_in_table[0]          # Shift or Reduce
         number = int(symbol_in_table[1:])    # 'S' 或 'r' 后的数字
 
         # Shift
@@ -135,27 +145,74 @@ def allControl(input_stack):
 
         # Reduce
         if action == 'r':
-            production_left  = grammar_rule[number][0]         # 归约产生式的左部列表
-            production_right = grammar_rule[number][1]         # 归约产生式的右部列表
+            len_production = len(grammar_rule[number])         # 产生式的长度
 
-            len_right = len(production_right)                       # 产生式右部的长度
+            if len_production == 2: # 产生式的长度为2，该产生式不带'|'
+                flag_or = False
+            else:                   # 产生式的长度大于2，该产生式带'|'
+                flag_or = True
 
-            # 弹出栈顶的 len_right 项
-            for i in range(len_right):
-                symbol_stack.pop()
-                state_stack.pop()
+            # 归约用的产生式不带'|'
+            if flag_or == False:
+                production_left  = grammar_rule[number][0]         # 归约产生式的左部列表
+                production_right = grammar_rule[number][1]         # 归约产生式的右部列表
 
-            symbol_stack.append(production_left)     # 将产生式左部压入符号栈中
+                len_right = len(production_right)                  # 产生式右部的长度
 
-            temp_current_state = state_stack[-1]     # 当前状态为状态栈栈顶元素
+                # 弹出栈顶的 len_right 项
+                for i in range(len_right):
+                    symbol_stack.pop()
+                    state_stack.pop()
 
-            # 遇到空白格，报错
-            if parsing_table[temp_current_state]['GOTO'].has_key(production_left) == False:
-                print 'error'
-                break
+                symbol_stack.append(production_left)     # 将产生式左部压入符号栈中
 
-            next_state = parsing_table[temp_current_state]['GOTO'][production_left] # 下一状态
-            state_stack.append(next_state)           # 将下一状态压入状态栈
+                temp_current_state = state_stack[-1]     # 当前状态为状态栈栈顶元素
+
+                # 遇到空白格，报错
+                if parsing_table[temp_current_state]['GOTO'].has_key(production_left) == False:
+                    print 'error'
+                    break
+
+                next_state = parsing_table[temp_current_state]['GOTO'][production_left] # 下一状态
+                state_stack.append(next_state)           # 将下一状态压入状态栈
+
+            else:   # 归约用的产生式带'|'
+                # 遍历产生式右部用'|'分割开的部分，寻找适合归约的产生式右部
+                for i in range(1, len_production):
+                    len_right = len(grammar_rule[number][i])     # 归约产生式第 i 个右部的长度
+                    len_symbol = len(symbol_stack)               # 符号栈的长度
+
+                    # 避免数组越界
+                    if len_right > (len_symbol-1):
+                        continue
+                    else:
+                        temp_production_right = ''.join(grammar_rule[number][i])    # 归约产生式第 i 个右部转成字符串
+                        temp_symbol_string    = ''.join(symbol_stack[-len_right:])  # 符号栈最后 len_right 个元素转成字符串
+
+                        if temp_production_right in temp_symbol_string:        # 用于归约的产生式右部
+                            production_left  = grammar_rule[number][0]         # 归约产生式的左部列表
+                            production_right = grammar_rule[number][i]         # 归约产生式的右部列表
+
+                            len_right = len(production_right)                  # 产生式右部的长度
+
+                            # 弹出栈顶的 len_right 项
+                            for j in range(len_right):
+                                symbol_stack.pop()
+                                state_stack.pop()
+
+                            symbol_stack.append(production_left)     # 将产生式左部压入符号栈中
+
+                            temp_current_state = state_stack[-1]     # 当前状态为状态栈栈顶元素
+
+                            # 遇到空白格，报错
+                            if parsing_table[temp_current_state]['GOTO'].has_key(production_left) == False:
+                                print 'error'
+                                break
+
+                            next_state = parsing_table[temp_current_state]['GOTO'][production_left] # 下一状态
+                            state_stack.append(next_state)           # 将下一状态压入状态栈
+
+                            break
 
             move = symbol_in_table + " Reduce"       # 当前动作为『归约』
 
